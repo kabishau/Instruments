61cb39262c86051cfc4788fdd19250eec7ed27fa
user131656 <user131656@ny218.local>
2017-11-18 15:01:55 -0500

    Refactoring by creating framework MusicKit and moving some classes in it with changing Access Control for classes, methods, and properties

--- Instruments/Instruments.swift
+++ Instruments/Instruments.swift
@@ -1,24 +1,4 @@
-class Instrument {
-    
-    let brand: String
-    
-    init(brand: String) {
-        self.brand = brand
-    }
-    
-    func tune() -> String {
-        fatalError("Implement this method for \(brand)")
-    }
-    
-    func play(_ music: Music) -> String {
-        return music.prepared()
-    }
-    
-    func perform(_ music: Music) {
-        print(tune())
-        print(play(music))
-    }
-}
+
 
 class Piano: Instrument {
     
@@ -52,18 +32,7 @@ class Piano: Instrument {
     }
 }
 
-class Music {
-    
-    let notes: [String]
-    
-    init(notes: [String]) {
-        self.notes = notes
-    }
-    
-    func prepared() -> String {
-        return notes.joined(separator: " ")
-    }
-}
+
 
 // instances of classes
 
