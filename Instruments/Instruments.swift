22c8dabffaf4aed0ec952ddb00b40e943d5a084b
user131656 <user131656@ny218.local>
2017-11-18 12:50:51 -0500

    3 classes have been created for representation of the concept of OOP

---
+++ Instruments/Instruments.swift
@@ -0,0 +1,55 @@
+class Instrument {
+    
+    let brand: String
+    
+    init(brand: String) {
+        self.brand = brand
+    }
+    
+    func tune() -> String {
+        fatalError("Implement this method for \(brand)")
+    }
+    
+    func play(_ music: Music) -> String {
+        return music.prepared()
+    }
+    
+    func perform(_ music: Music) {
+        print(tune())
+        print(play(music))
+    }
+}
+
+class Piano: Instrument {
+    
+    let hasPedals: Bool
+    static let whiteKeys = 52
+    static let blackKeys = 36
+    
+    init(hasPedals: Bool = false, brand: String) {
+        self.hasPedals = hasPedals
+        super.init(brand: brand)
+    }
+    
+    override func tune() -> String {
+        return "Piano standard tuning for \(brand)."
+    }
+    
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Piano playing \(preparedNotes)"
+    }
+}
+
+class Music {
+    
+    let notes: [String]
+    
+    init(notes: [String]) {
+        self.notes = notes
+    }
+    
+    func prepared() -> String {
+        return notes.joined(separator: " ")
+    }
+}
